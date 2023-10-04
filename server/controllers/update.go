4044d28d562053773dcdb43afcce074cf1a61445
David Barth <76754802+david-brt@users.noreply.github.com>
2023-10-04 22:42:22 +0200

    split files
    makes controllers more manageable, done for future proofing

--- server/controllers/update.go
+++ server/controllers/update.go
@@ -2,25 +2,9 @@ package controllers
 
 import (
 	"database/sql"
-	"log"
-	"messageinabottle/models/compounds"
-
 	"github.com/gofiber/fiber/v2"
+	"messageinabottle/models/compounds"
 )
-
-func IndexHandler(c *fiber.Ctx, db *sql.DB) error {
-	message := compounds.UserMessage{}
-
-	row := db.QueryRow("SELECT m.id, m.content, m.timestamp, u.id, u.username FROM Message m JOIN User_ u ON m.sender = u.id ORDER BY timestamp DESC")
-	err := row.Scan(&message.MessageID, &message.Content, &message.Timestamp, &message.SenderID, &message.SenderName)
-
-	if err != nil {
-		log.Fatalln("No rows returned.")
-		return c.Status(500).JSON("error fetching from database")
-	}
-
-	return c.JSON(message)
-}
 
 func UpdateHandler(c *fiber.Ctx, db *sql.DB) error {
 	message := compounds.UserMessage{}
