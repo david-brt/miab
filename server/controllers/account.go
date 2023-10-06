a0db281a099e34250c9cb1d0f6d48ca36dac6fdc
David Barth <76754802+david-brt@users.noreply.github.com>
2023-10-06 19:17:19 +0200

    add login handler
    add the ability to generate a jwt token for a given user

--- server/controllers/account.go
+++ server/controllers/account.go
@@ -2,14 +2,18 @@ package controllers
 
 import (
 	"database/sql"
-	"github.com/gofiber/fiber/v2"
 	"messageinabottle/models"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func SignupHandler(c *fiber.Ctx, db *sql.DB) error {
 	user := models.User{}
 	if err := c.BodyParser(&user); err != nil {
-		return c.Status(400).JSON(err.Error())
+		return c.Status(500).JSON(err.Error())
 	}
 
 	statement := `INSERT INTO User_ (username) VALUES ($1)`
@@ -18,5 +22,32 @@ func SignupHandler(c *fiber.Ctx, db *sql.DB) error {
 	if err != nil {
 		return c.Status(500).JSON("Could not insert into database.")
 	}
-	return c.Status(200).JSON("User created successfully.")
+	return c.JSON("User created successfully.")
+}
+
+// generates jwt token and returns it to client
+func LoginHandler(c *fiber.Ctx, db *sql.DB) error {
+	user := models.User{}
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+
+	expirationDate := time.Now().Add(time.Hour * 24 * 14)
+	expirationDateNumeric := jwt.NumericDate{Time: expirationDate}
+	currentTimeNumeric := jwt.NumericDate{Time: time.Now()}
+
+	key := []byte(os.Getenv("JWT_SECRET"))
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"sub":  user.ID,
+			"name": user.Username,
+			"exp":  expirationDateNumeric,
+			"iat":  currentTimeNumeric,
+		})
+	s, err := t.SignedString(key)
+	if err != nil {
+		return c.Status(500).JSON("An error occurred generating authentication token.")
+	}
+
+	return c.JSON(s)
 }
--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
 	"messageinabottle/controllers"
@@ -11,12 +12,24 @@ import (
 )
 
 func main() {
+	env := os.Getenv("MIAB_ENV")
+	if env == "" {
+		env = "development"
+		err := godotenv.Load(".env.local")
+		if err != nil {
+			log.Fatal("Error loading .env file.")
+		}
+	}
+
 	app := fiber.New()
 	connStr := "postgresql://postgres:postgres@db/messageinabottle?sslmode=disable"
 	db := services.ConnectDB(connStr)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return controllers.IndexHandler(c, db)
+	})
+	app.Get("/login", func(c *fiber.Ctx) error {
+		return controllers.LoginHandler(c, db)
 	})
 	app.Post("/update", func(c *fiber.Ctx) error {
 		return controllers.UpdateHandler(c, db)
@@ -26,8 +39,5 @@ func main() {
 	})
 
 	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3030"
-	}
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
 }
