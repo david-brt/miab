92b1e72c31e3258d76d53ac17af76251ef4249d8
Marvin <marvin@hofmannwelt.de>
2023-10-18 23:44:26 +0200

    signupform von loginform kopiert

---
+++ frontend/src/lib/SuignupForm.svelte
@@ -0,0 +1,21 @@
+<form class="modal-form" method="POST" action="?/signup">
+	<label for="username-input" class="form-label">name</label>
+	<input
+		name="username"
+		id="username-input"
+		type="text"
+		placeholder="username"
+		maxlength="34"
+		class="form-input"
+	/>
+	<label for="password-input" class="form-label">password</label>
+	<input
+		name="password"
+		id="password-input"
+		type="password"
+		placeholder="password"
+		maxlength="72"
+		class="form-input"
+	/>
+	<button class="send-button">send</button>
+</form>
