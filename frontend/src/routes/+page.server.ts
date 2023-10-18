4780f55757a213cdd95b77c5b02dffadc46ba3a7
David Barth <76754802+barthdavid@users.noreply.github.com>
2023-10-19 00:07:09 +0200

    remove unused action scripts

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,7 +1,6 @@
 import { INTERNAL_DATA_ROUTE } from '$env/static/private';
 import type { LoadEvent } from '@sveltejs/kit';
 import type { Message } from '../../types/message';
-import type { Actions } from './$types';
 
 export async function load({ fetch }: LoadEvent) {
 	const response = await fetch(INTERNAL_DATA_ROUTE);
@@ -10,46 +9,3 @@ export async function load({ fetch }: LoadEvent) {
 		message
 	};
 }
-
-export const actions: Actions = {
-	message: async ({ request }) => {
-		const data = await request.formData();
-		const jsonData = {
-			senderName: data.get('senderName'),
-			content: data.get('content')
-		};
-		const url = `${INTERNAL_DATA_ROUTE}/send-message`;
-		const response = await fetch(url, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(jsonData)
-		});
-	} /*,
-	login: async ({ request, cookies }) => {
-		const data = await request.formData();
-		const jsonData = {
-			username: data.get('username'),
-			password: data.get('password')
-		};
-		const url = `${PUBLIC_DATA_ROUTE}/login`;
-		const response = await fetch(url, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(jsonData)
-		});
-		if (response.status === 202) {
-			const { auth_token } = await response.json();
-			cookies.set('auth_token', auth_token);
-			return { success: true, validCredentials: true };
-		} else if (response.status === 401 || response.status === 404) {
-			return { success: true, validCredentials: false, formData: jsonData };
-		} else {
-			return { success: false, validCredentials: false, formData: jsonData };
-		}
-	}
-  */
-};
