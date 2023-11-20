28b29e687b42c625c27adf7af14a88beb9126ab1
David Barth <76754802+barthdavid@users.noreply.github.com>
2023-11-20 16:01:34 +0100

    add dependencies
    yup was not added as dependency in last commit

--- frontend/src/lib/schema/singupSchema.ts
+++ frontend/src/lib/schema/singupSchema.ts
@@ -1,13 +1,13 @@
-import { object, string, ref } from "yup"
-import { passwordString } from "./passwordString"
-import { usernameString } from "./usernameString"
+import { object, string, ref } from 'yup';
+import { passwordString } from './password';
+import { usernameString } from './username';
 
 export const signupSchema = object({
   username: usernameString,
   password: passwordString,
   confirmPassword: string()
-    .required("Please re-type your password")
+    .required('Please re-type your password')
     // oneOf matches one of the values inside the array.
     // "ref" gets the value of password.
-    .oneOf([ref("password")], "Passwords do not match"),
-})
+    .oneOf([ref('password')], 'Passwords do not match')
+});
