6039d43a09152d593455c33aea8fe1b8515e67ec
David Barth <76754802+barthdavid@users.noreply.github.com>
2023-11-22 14:54:01 +0100

    move back to form actions
    very buggy, see https://github.com/pablo-abc/felte/issues/277 for more info

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,6 +1,9 @@
 import { INTERNAL_DATA_ROUTE } from '$env/static/private';
-import type { LoadEvent } from '@sveltejs/kit';
+import { fail, type LoadEvent } from '@sveltejs/kit';
 import type { Message } from '../../types/message';
+import type { Actions } from '@sveltejs/kit';
+
+const CREATED = 201;
 
 export async function load({ fetch }: LoadEvent) {
   const response = await fetch(`${INTERNAL_DATA_ROUTE}/global-message`);
@@ -9,3 +12,34 @@ export async function load({ fetch }: LoadEvent) {
     message
   };
 }
+
+export const actions: Actions = {
+  signup: async ({ request }) => {
+    const formData = await request.formData();
+    const formBody = {
+      username: formData.get('username'),
+      password: formData.get('password')
+    };
+
+    const headers: Record<string, string> = {
+      'Content-Type': 'application/json'
+    };
+
+    const response = await fetch(`${INTERNAL_DATA_ROUTE}/signup`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: headers,
+      body: JSON.stringify(formBody),
+      credentials: 'include'
+    });
+
+    const responseData = await response.json();
+    const signupStatus = response.status;
+
+    if (signupStatus === CREATED) {
+      return { success: true };
+    }
+
+    return fail(signupStatus, responseData);
+  }
+};
